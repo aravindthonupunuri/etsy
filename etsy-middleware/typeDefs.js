2722f182397de1592eaf5fca98583474a6c67fd1
aravindthonupunuri <aravindthonupunuri75@gmail.com>
2022-05-14 19:52:00 +0000

    updating typeDefs

--- etsy-middleware/typeDefs.js
+++ etsy-middleware/typeDefs.js
@@ -3,86 +3,86 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
   type User {
     _id: ID!
-    username: String!
-    emailId: String!
-    password: String!
-    phonenumber: String
-    profilePicture: String
-    gender: String
-    city: String
-    country: String
-    dateofbirth: Date
-    address: String
+    username: String!,
+    emailId: String!,
+    password: String!,
+    phonenumber: String,
+    profilePicture: String,
+    gender: String,
+    city: String,
+    country: String,
+    dateofbirth: Date,
+    address: String,
     about: String
   }
 
   type Favourites {
-    itemId: String!
-    userId: String!
+    itemId: String!,
+    userId: String!,
   }
 
   type Shop {
-    _id: ID!
-    shopownerId: String!
-    shopname: String!
-    shopimage: String
+    _id: ID!,
+    shopownerId: String!,
+    shopname: String!,
+    shopimage: String,
     salescount: Int
   }
 
   type Item {
-    _id: ID!
-    itemname: String!
-    shopname: String!
-    itemimage: String
-    description: String
-    price: Float
-    available_quantity: Int
+    _id: ID!,
+    itemname: String!,
+    shopname: String!,
+    itemimage: String,
+    description: String,
+    price: Float,
+    available_quantity: Int,
     category: String
   }
 
   type Cart {
-    _id: ID!
-    itemid: ID!
-    userid: ID!
-    quantity: Int!
+    _id: ID!,
+    itemid: ID!,
+    userid: ID!,
+    quantity: Int!,
     shopname: Date!
   }
 
   type Order {
-    _id: ID!
-    userid: ID!
-    total: Int!
+    _id: ID!,
+    userid: ID!,
+    total: Int!,
     createdtime: Date
   }
 
   type OrderItem {
-    _id: ID!
-    orderid: ID!
-    itemid: ID!
-    price: String
-    quantity: String
-    message: String
-    shopname: String
+    _id: ID!,
+    orderid: ID!,
+    itemid: ID!,
+    price: String,
+    quantity: String,
+    message: String,
+    shopname: String,
     createdtime: Date
   }
 
   type Query {
-    getUserById(userId: ID): User
-    getAllShop: Shop
-    getShopByShopName(shopName: String): Shop
-    getShopOwnerDetails(shopOwnerId: ID): User
-    getShopItems(shopname: String): [Item]
-    getUserProfile(userId: ID): User
-    getFavourites(userId: ID): [Favourites]
-    getUserOrders(userId: ID)
-    getItems(): [Item]
-    getItemsByItemId(itemId: ID): Item
+    getUserById(userId: ID): User,
+    getAllShop: Shop,
+    getShopByShopName(shopName: String): Shop,
+    getShopOwnerDetails(shopOwnerId: ID): User,
+    getShopItems(shopname: String): [Item],
+    getUserProfile(userId: ID): User,
+    getFavourites(userId: ID): [Favourites],
+    getUserOrders(userId: ID),
+    getItems(): [Item],
+    getItemsByItemId(itemId: ID): Item,
     getItemsByUserId(userId: String): [Item]
   }
 
   type Mutation {
-    registerUser(emailId: String!, username: String!, password: String!): User
-    loginUser(emailId: String!, password: String!): User
+    registerUser(emailId: String!, username: String!, password: String!): User,
+    loginUser(emailId: String!, password: String!): User,
     addItem(
       itemname: String!,
       shopname: String!,
@@ -91,7 +91,7 @@ const typeDefs = gql`
       price: String,
       available_quantity: Int,
       category: String,
-    ): Item
+    ): Item,
     updateUserDetails(
       phonenumber: String
       dateofbirth: String
@@ -100,8 +100,8 @@ const typeDefs = gql`
       address: String
       city: String
       country: String
-    ): User
-    createShop(shopName: String): Shop
+    ): User,
+    createShop(shopName: String): Shop,
     addItemToShop(
       itemname: String!,
       shopname: String!,
@@ -110,7 +110,7 @@ const typeDefs = gql`
       price: String,
       available_quantity: Int,
       category: String
-    ) : Item
+    ) : Item,
     updateItem(
       itemname: String!,
       itemimage: String,
@@ -118,8 +118,8 @@ const typeDefs = gql`
       price: String,
       available_quantity: Int,
       category: String
-    ) : Item
-    createOrder( price: Int!, userId: ID!): Order
+    ) : Item,
+    createOrder( price: Int!, userId: ID!): Order,
     createOrderItems(orderId: ID!, orderItems: [Items]): OrderItems
   }
 `;
